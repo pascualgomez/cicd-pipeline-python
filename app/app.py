83ad363c1e525f75cd36d13a9941c14f6810e3ed
Pascual Gómez <pascual.gomez@offerup.com>
2025-09-13 01:43:00 -0500

    fix issues reported by sonarqube

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+"""This module contains the main application logic."""
 # app/app.py
 from flask import Flask, render_template, request
 from .calculadora import sumar, restar, multiplicar, dividir
@@ -7,6 +8,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    """Renders the main calculator page and handles the calculation logic.
+
+    On a GET request, it displays the calculator's user interface.
+    On a POST request, it processes the submitted form data to perform
+    a mathematical operation (addition, subtraction,
+    multiplication, or division)on two numbers.
+    It handles invalid number inputs and division by zero.
+
+    Returns:
+        The rendered HTML template ('index.html') with the context of
+        the calculation result.
+    """
     resultado = None
     if request.method == "POST":
         try:
@@ -33,4 +46,5 @@ def index():
 
 
 if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
+    # Quita debug=True para producción
+    app.run(debug=True, port=5000, host="0.0.0.0")
--- app/calculadora.py
+++ app/calculadora.py
@@ -1,17 +1,64 @@
 # app/calculadora.py
+"""This module provides basic arithmetic operations.
+
+    It includes functions for addition,
+    subtraction, multiplication, and division,
+    including error handling for division by zero.
+"""
+
+
 def sumar(a, b):
+    """Returns the sum of two numbers.
+
+    Args:
+        a (float or int): The first number.
+        b (float or int): The second number.
+
+    Returns:
+        float or int: The sum of a and b.
+    """
     return a + b
 
 
 def restar(a, b):
+    """Returns the difference between two numbers.
+
+    Args:
+        a (float or int): The first number (minuend).
+        b (float or int): The second number (subtrahend).
+
+    Returns:
+        float or int: The result of subtracting b from a.
+    """
     return a - b
 
 
 def multiplicar(a, b):
+    """Returns the product of two numbers.
+
+    Args:
+        a (float or int): The first number.
+        b (float or int): The second number.
+
+    Returns:
+        float or int: The product of a and b.
+    """
     return a * b
 
 
 def dividir(a, b):
+    """Returns the division of two numbers.
+
+    Args:
+        a (float or int): The numerator.
+        b (float or int): The denominator.
+
+    Returns:
+        float: The result of dividing a by b.
+
+    Raises:
+        ZeroDivisionError: If the denominator (b) is zero.
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
